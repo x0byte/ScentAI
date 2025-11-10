48ed22d9f9588e7f0f02ce5fc9e82e7a4ad51617
Hirun Weththewa <hirunwe@gmail.com>
2025-11-11 02:55:55 +1100

    Add note extraction functionality with zero-shot classification and CSV data

---
+++ API/routes/extract_notes.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+from transformers import pipeline
+
+current_dir = os.path.dirname(__file__)  # Directory of the current script
+data_file_path = os.path.join(current_dir, "../../Data/notes_and_family.csv")
+
+notes_df = pd.read_csv(data_file_path)
+
+candidate_notes = notes_df["Note"].tolist()
+
+classifier = pipeline("zero-shot-classification",
+                      model="facebook/bart-large-mnli",
+                      device=-1)   # CPU
+
+text = ""
+
+def extract_notes(description: str, candidate_notes, threshold=0.4):
+
+    '''Returns notes relevent to the given scent description'''
+
+    result = classifier(
+        description,
+        candidate_notes = candidate_notes,
+        multi_label=True
+    )
+
+    filtered = [
+        (label, score) 
+        for label, score in zip(result["labels"], result["scores"]) 
+        if score >= threshold
+    ]
+
+    filtered.sort(key=lambda x: x[1], reverse=True)
+    return filtered
